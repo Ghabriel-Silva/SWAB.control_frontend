f7f9b67218f147bf0b039f7c4a9bdf4fddcb8b5e
Gabriel <ghaabriieel@gmail.com>
2026-06-20 18:08:17 -0300

    refatorando dialog swab deixaando resposivo, refatorando texto

---
+++ .history/src/app/(private)/swab/components/dialog/InfoStatic_20260620163739.tsx
@@ -0,0 +1,78 @@
+import { Badge, SimpleGrid } from "@chakra-ui/react";
+import { BodyText, FormField, InfoNull, SubtitleText } from "@/app/(private)/components/index";
+import { format } from "date-fns";
+import { defineColorTypeSwab } from "@/app/(private)/utils/defineColorTypeSwab";
+import { SwabDataProps } from "../../types/swab.dataProps";
+
+
+export const InfoStatic = ({ swab }: SwabDataProps) => {
+    return (
+        <SimpleGrid
+            border={"1px solid"}
+            borderColor={"gray.200"}
+            p={4}
+            borderRadius={"sm"}
+            columns={{ base: 2, md: 3, lg: 3 }}
+            gap={4}
+            w="100%"
+        >
+            <FormField label="Lote">
+                <SubtitleText>{swab?.internalCode}</SubtitleText>
+            </FormField>
+            <FormField label="Tanque">
+                {swab.location.name ? (
+                    <SubtitleText>
+                        {swab.location.name}
+                    </SubtitleText>
+                ) : (
+                    <InfoNull />
+                )
+                }
+            </FormField>
+
+            <FormField label="Toneira">
+                {swab.faucetCode ? (
+                    <SubtitleText>
+                        {swab.location.name}
+                    </SubtitleText>
+                ) : (
+                    <InfoNull />
+                )
+                }
+            </FormField>
+            <FormField label="Criado em">
+                {swab.location.name ? (
+                    <SubtitleText>
+                        {format(swab.createdAt, 'dd/MM/yyyy HH:mm')}
+                    </SubtitleText>
+                ) : (
+                    <InfoNull />
+                )
+                }
+            </FormField>
+
+            <FormField label="Atualizado em">
+                {swab.location.name ? (
+                    <SubtitleText>
+                        {format(swab.updatedAt, 'dd/MM/yyyy HH:mm')}
+                    </SubtitleText>
+                ) : (
+                    <InfoNull />
+                )
+                }
+            </FormField>
+            <FormField label="Tipo Swab">
+                <Badge colorPalette={defineColorTypeSwab(swab.check.type)}>
+                    {swab.location.name ? (
+                        <SubtitleText>
+                            {swab.check.type}
+                        </SubtitleText>
+                    ) : (
+                        <InfoNull />
+                    )
+                    }
+                </Badge>
+            </FormField>
+        </SimpleGrid>
+    )
+}
